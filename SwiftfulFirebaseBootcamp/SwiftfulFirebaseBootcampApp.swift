5ec5229b721631d3fb0028450ab3ac18e2d5d235
Yeabsira <Yeabsirasefr@gmail.com>
2024-02-22 12:04:32 -0500

    Initial Commit

---
+++ SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulFirebaseBootcampApp.swift
+//  SwiftfulFirebaseBootcamp
+//
+//  Created by Yeabsira Sefr on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
